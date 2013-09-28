041451a5934548ae50f942c340575e2d0b0ea1ca
teyler <teyler@teylers-MacBook-Pro.local>
2013-09-27 20:36:52 -0700

    Edited Gitignore DS_Store

---
+++ social/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Social::Application.config.secret_key_base = 'b1606703bb7485c4cdcb1ebd101569b01e38f9b5e76a8dac39ea8a59897d38769e70c549eb6a82cca539251ab99f8638438a1e1962d538ce9fe21e4bfb759d2c'
